1dff377777ac1d936c42f26fc7179511c5852141
Alvin Truong <Atruong13@bostonk12.org>
2024-03-08 09:28:08 -0500

    Python practice version.2

---
+++ Python/Week 13/2024-03-07/Practice2.py
@@ -0,0 +1,13 @@
+#Alvin Truong
+# 3/8/2024
+
+name = input("Please enter your name?:")
+food = input ("please enter favorite food?:")
+song = input ("Please enter your favorite song?:")
+quote = input ("Please enter your favorite quote?")
+print(f"Infomation about{name},")
+print(f"*****")
+print(f"Favorite food:{food}")
+print(f"Favorite song:{song}")
+print(f"Favorite quote:{quote}")
+print(f"Summary: {name}'s favorite food is {food}, their favorite song is \"{song}\" and their favorite quote is \"{quote}\".")
