312b8be93c754407b4d2594ec85106a18225a83a
Alvin Truong <Atruong13@bostonk12.org>
2024-05-08 10:18:45 -0400

    Python practice version.2

---
+++ Python/Week_17/05-08-2024/list.py
@@ -0,0 +1,12 @@
+#Alvin Truong
+#5/8/2024
+
+#Creating an empty list
+lst = []
+
+#using a four loop to add the numbers 1 through 100, to the list
+for a in range (1,101):
+    lst.append(a)
+
+#printing the list
+print(lst)
