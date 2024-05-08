ef97916424229fd9efd9102d3b5120f515ce38dc
Alvin Truong <Atruong13@bostonk12.org>
2024-05-08 07:33:37 -0400

    Python practice version.2

---
+++ Python/Week_17/5-7-2024/check.py
@@ -0,0 +1,24 @@
+#Alvin Truong
+#5/7/2024
+
+#Creating an empty list
+a = []
+
+#using the operator to add to the list
+a += [1]
+a += [2]
+a += [3]
+
+#printing the list
+print(a)
+
+#creating an empty list named "b
+b = []
+
+#using the function to add to the list "b"
+b.append(1)
+b.append(2)
+b.append(3)
+
+#printing the list "b"
+print(b)
