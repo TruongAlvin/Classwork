4f025a5209a17036bca8daf42f03b1ab6590609e
Alvin Truong <Atruong13@bostonk12.org>
2024-06-17 14:13:19 -0400

    Python practice version.2

--- Python/Week_20/Final_Exam/final.py
+++ Python/Week_20/Final_Exam/final.py
@@ -1,4 +1,5 @@
 def main():
+    print(Subword("word", "wordle"))
     words = []
     with open("dictionary.txt", 'r') as f:
         for line in f:
@@ -6,6 +7,22 @@ def main():
     words.sort()
     print(words)
     print("YABE" in words)
+    words_dict={3: [],4: [] ,5: [],6: [],7: [] }
+    for word in words:
+        words_dict[len(word)].append(word)
+    print(words_dict)
+def Subword(word_a,word_b):
+    for letter in word_a:
+        if word_a.count(letter) > word_b.count(letter):
+            return False
+    return True
+def superword(superwords, word_dict):
+    subwords = []
+    for length, words in word_dict.items():
+        for word in words:
+            if Subword(words,superwords):
+                subwords.append(word)
+    return subwords
 
 
 if __name__ == '__main__':
